a6e06abe1949bae84090dafff761c8c700a684b7
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2024-11-22 22:05:42 -0800

    Adding Support for Newlines to Terminal Driver

--- kernel.cpp
+++ kernel.cpp
@@ -117,7 +117,7 @@ extern "C" {
 		/* Initialize terminal interface */
 		terminal_initialize();
 
-		/* Newline support is left as an exercise. */
 		terminal_writestring("Hello, kernel World!\n");
+		terminal_writestring("Hello, kernel World, second line!\n");
 	}
 }
