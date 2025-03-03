5bb7ddf18a3e88f497541235f3aeddc28b13c6ec
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2025-03-03 08:25:12 -0800

    GDT Implementation

---
+++ kernel/arch/i386/gdt.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+
+#include <kernel/gdt.h>
+
+#define GDT_ENTRIES 5
+
+#define GDT_ACCESS_PRESENT        0x80
+#define GDT_ACCESS_RING0          0x00
+#define GDT_ACCESS_RING3          0x60
+#define GDT_ACCESS_EXECUTABLE     0x08
+#define GDT_ACCESS_CODE_OR_DATA   0x10
+#define GDT_ACCESS_WRITABLE       0x02
+
+#define GDT_FLAG_4K_GRANULARITY   0x80
+#define GDT_FLAG_32_BIT           0x40
+
+struct gdt_entry {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t access;
+	uint8_t granularity;
+	uint8_t base_high;
+} __attribute__((packed));
+
+struct gdt_ptr {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+
+struct gdt_entry gdt[GDT_ENTRIES];
+struct gdt_ptr gdtp;
+
+static void encode_gdt_entry(struct gdt_entry* entry, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
+	entry->limit_low = limit & 0xFFFF;
+	entry->base_low = base & 0xFFFF;
+	entry->base_middle = (base >> 16) & 0xFF;
+	entry->access = access;
+	entry->granularity = (limit >> 16) & 0x0F;
+	entry->granularity |= (flags & 0xF0);
+	entry->base_high = (base >> 24) & 0xFF;
+}
+
+void gdt_initialize() {
+	gdtp.limit = sizeof(gdt) - 1;
+	gdtp.base = (uint32_t)&gdt;
+
+	encode_gdt_entry(&gdt[0], 0, 0, 0, 0);
+	encode_gdt_entry(&gdt[1], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	encode_gdt_entry(&gdt[2], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	encode_gdt_entry(&gdt[3], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	encode_gdt_entry(&gdt[4], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+
+	asm volatile ("lgdt (%0)" : : "r" (&gdtp));
+}
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
 void kernel_main(void) {
+	gdt_initialize();
+	
 	terminal_initialize();
 	printf("Hello, kernel World!\n\tsecond lind\be\vthird line\a");
 }
