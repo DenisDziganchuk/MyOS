406f4539c17f0df64858c5bb8624c315054f9004
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2025-03-03 14:56:30 -0800

    Partial rewrite and restructure of GDT

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -14,24 +14,14 @@
 #define GDT_FLAG_4K_GRANULARITY   0x80
 #define GDT_FLAG_32_BIT           0x40
 
-struct gdt_entry {
-	uint16_t limit_low;
-	uint16_t base_low;
-	uint8_t base_middle;
-	uint8_t access;
-	uint8_t granularity;
-	uint8_t base_high;
-} __attribute__((packed));
-
-struct gdt_ptr {
-	uint16_t limit;
-	uint32_t base;
-} __attribute__((packed));
-
 struct gdt_entry gdt[GDT_ENTRIES];
 struct gdt_ptr gdtp;
 
-static void encode_gdt_entry(struct gdt_entry* entry, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
+void gdt_flush() {
+	asm volatile ("lgdt (%0)" : : "r" (&gdtp));
+}
+
+void gdt_set_entry(struct gdt_entry* entry, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
 	entry->limit_low = limit & 0xFFFF;
 	entry->base_low = base & 0xFFFF;
 	entry->base_middle = (base >> 16) & 0xFF;
@@ -45,11 +35,11 @@ void gdt_initialize() {
 	gdtp.limit = sizeof(gdt) - 1;
 	gdtp.base = (uint32_t)&gdt;
 
-	encode_gdt_entry(&gdt[0], 0, 0, 0, 0);
-	encode_gdt_entry(&gdt[1], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
-	encode_gdt_entry(&gdt[2], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
-	encode_gdt_entry(&gdt[3], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
-	encode_gdt_entry(&gdt[4], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	gdt_set_entry(&gdt[0], 0, 0, 0, 0);
+	gdt_set_entry(&gdt[1], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	gdt_set_entry(&gdt[2], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	gdt_set_entry(&gdt[3], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_EXECUTABLE | GDT_ACCESS_CODE_OR_DATA, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
+	gdt_set_entry(&gdt[4], 0, 0xFFFFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING3 | GDT_ACCESS_CODE_OR_DATA | GDT_ACCESS_WRITABLE, GDT_FLAG_4K_GRANULARITY | GDT_FLAG_32_BIT);
 
-	asm volatile ("lgdt (%0)" : : "r" (&gdtp));
+	gdt_flush();
 }
