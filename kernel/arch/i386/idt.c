2d7e4e4d0c566bc4d2f92d1f2e1261f8ae48911d
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2025-03-03 17:04:15 -0800

    IDT Implementation

---
+++ kernel/arch/i386/idt.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <string.h>
+
+#include <kernel/idt.h>
+
+#define IDT_ENTRIES 256
+
+struct idt_entry idt[IDT_ENTRIES];
+struct idt_ptr idtp;
+
+void idt_load() {
+	asm volatile ("lidt %0" : : "m" (idtp));
+}
+
+void idt_set_entry(struct idt_entry* entry, uint32_t base, uint16_t selector, uint8_t flags) {
+	entry->base_low = base & 0xFFFF;
+	entry->base_high = (base >> 16) & 0xFFFF;
+	entry->selector = selector;
+	entry->zero = 0;
+	entry->flags = flags | 0x60;
+}
+
+void idt_initialize() {
+	idtp.limit = sizeof(idt) - 1;
+	idtp.base = (uint32_t)&idt;
+	memset(&idt, 0, sizeof(idt));
+	
+	idt_load();
+}
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,9 +2,11 @@
 
 #include <kernel/tty.h>
 #include <kernel/gdt.h>
+#include <kernel/idt.h>
 
 void kernel_main(void) {
 	gdt_initialize();
+	idt_initialize();
 	
 	terminal_initialize();
 	printf("Hello, kernel World!\n\tsecond lind\be\vthird line\a");
